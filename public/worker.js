5d856028fed3de076371df05107021dc479db91d
Roshan Kr Soni <roshankrsoni1@gmail.com>
2021-01-07 15:03:46 +0530

    sw updated

--- public/worker.js
+++ public/worker.js
@@ -1,48 +1,61 @@
 var CACHE_NAME = 'pwa-task-manager';
 var urlsToCache = [
   '/',
-  '/todo/:id'
+  '/todos/:id',
+  '/index.html',
+  '/worker.js'
 ];
 
-// Install a service worker
-self.addEventListener('install', event => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+  evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+          console.log('[ServiceWorker] Pre-caching offline page');
+          return cache.addAll(urlsToCache);
+      }).then(self.skipWaiting())
+  );
+
+
+});
+
+self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
+
+  evt.waitUntil(
+      caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+              if (key !== CACHE_NAME) {
+                  console.log('[ServiceWorker] Removing old cache', key);
+                  return caches.delete(key);
+              }
+          }));
       })
   );
-});
 
-// Cache and return requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+  self.clients.claim();
 });
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
-// Update a service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['pwa-task-manager'];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+workbox.googleAnalytics.initialize();
+
+self.addEventListener('fetch', (evt) => {
+  console.log('[ServiceWorker] Fetch', evt.request.url);
+  if (evt.request.url.includes('nariohtools.com/')) {
+      evt.respondWith(
+
+          caches.open(CACHE_NAME).then(cache => {
+              return cache.match(evt.request).then(cacheResponse => cacheResponse || fetch(evt.request).then(networkResponse => {
+                  cache.put(evt.request, networkResponse.clone());
+                  return networkResponse;
+              }));
+
+          }))
+
+  } else {
+      evt.respondWith(
+          fetch(evt.request)
+      )
+  }
+
+
 });
