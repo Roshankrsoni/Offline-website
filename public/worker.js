3a17e38ae8369175972172a8ea7cd7a028c50142
Roshan Kr Soni <roshankrsoni1@gmail.com>
2021-01-07 16:18:14 +0530

    updated

--- public/worker.js
+++ public/worker.js
@@ -1,61 +1,33 @@
-var CACHE_NAME = 'pwa-task-manager';
-var urlsToCache = [
+// give your cache a name
+const cacheName = 'my-cache';
+
+// put the static assets and routes you want to cache here
+const filesToCache = [
   '/',
-  '/todos/:id',
+  '/todos',
   '/index.html',
-  '/worker.js'
+  '/todos/:id',
+  '/logo512.png',
+  '/logo192.png',
 ];
 
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-  evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-          console.log('[ServiceWorker] Pre-caching offline page');
-          return cache.addAll(urlsToCache);
-      }).then(self.skipWaiting())
+// the event handler for the activate event
+self.addEventListener('activate', e => self.clients.claim());
+
+// the event handler for the install event 
+// typically used to cache assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll(filesToCache))
   );
-
-
 });
 
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-
-  evt.waitUntil(
-      caches.keys().then((keyList) => {
-          return Promise.all(keyList.map((key) => {
-              if (key !== CACHE_NAME) {
-                  console.log('[ServiceWorker] Removing old cache', key);
-                  return caches.delete(key);
-              }
-          }));
-      })
-  );
-
-  self.clients.claim();
-});
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-
-workbox.googleAnalytics.initialize();
-
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  if (evt.request.url.includes('nariohtools.com/')) {
-      evt.respondWith(
-
-          caches.open(CACHE_NAME).then(cache => {
-              return cache.match(evt.request).then(cacheResponse => cacheResponse || fetch(evt.request).then(networkResponse => {
-                  cache.put(evt.request, networkResponse.clone());
-                  return networkResponse;
-              }));
-
-          }))
-
-  } else {
-      evt.respondWith(
-          fetch(evt.request)
-      )
-  }
-
-
+// the fetch event handler, to intercept requests and serve all 
+// static assets from the cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+    .then(response => response ? response : fetch(e.request))
+  )
 });
